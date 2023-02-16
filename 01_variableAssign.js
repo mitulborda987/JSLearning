4f830607d5f3634c9530131359ace2cbd0c97143
mitulborda987 <mitulborda987@gmail.com>
2023-02-16 15:45:52 +0530

    helloworld.js and variableassign.js file updated
    helloworld.js and variableassign.js file updated

---
+++ 01_variableAssign.js
@@ -0,0 +1,44 @@
+console.log("************************solution:1************************");
+
+console.log(">>>>>>>>>>  Before Initialize Variable <<<<<<<<<<<");
+var myfullname;
+console.log(myfullname);
+
+console.log(">>>>>>>>>>  After Initialize Variable <<<<<<<<<<<");
+myfullname = "Mitul Borda";
+console.log("My fullname is: " + myfullname);
+
+console.log(">>>>>>>>>>  Update FullName <<<<<<<<<<<");
+myfullname = "Mitul pradipbhai borda"
+console.log("My fullname with middle name: " + myfullname);
+
+console.log("************************solution:2************************");
+var myLove = "i love only JS"
+console.log(myLove);
+
+console.log("************************solution:3************************");
+var firstName = "Mitul";
+console.log("My first name is: " + firstName);
+var lastName = "Borda";
+console.log("My last name is: " + lastName);
+
+var age = "25";
+console.log("My age is: " + age);
+
+var address = "Ahemdabad";
+console.log("My address is: " + address);
+
+var pincode = "123456";
+console.log("My age is: " + pincode);
+
+var gender = "Male";
+console.log(gender);
+
+var ismarried = "Unmarried";
+console.log("My marital is: " + ismarried);
+
+console.log("******************single line Output******************");
+
+console.log("My first name is: " + firstName, lastName, age, address, gender, ismarried);
+
+ 
--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,27 @@
-console.log(" Mitul Borda ");
-console.log(5*85);
+console.log('"Mitul Borda"');
+console.log('31');
+
+var fullName = "pune"
+console.log(fullName);
+var city;
+console.log(city);
+city ="pune";
+console.log(city);
+city = "surat";
+console.log(city);
+
+var age="31"
+ age="32"
+age="33"
+console.log(age);
+ var university;
+ university = "SGBAU"
+ var university 
+
+ var firstname = "firstname";
+
+//  var lastname ="lastname";
+//  console.log(firstname +"|" + lastname);
+// //  var male = "male";
+// //  var borda = "borda";
+//  console.log(male    +"|" + borda);
