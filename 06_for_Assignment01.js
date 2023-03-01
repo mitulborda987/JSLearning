4859729f58f24072af38ae29e72eb3315fa5600f
mitulborda987 <mitulborda987@gmail.com>
2023-03-01 11:01:52 +0530

    incerement decrement assignment completed
    incerement decrement assignment completed

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,36 @@
+console.log(`WAP to print numbers from 5 to 15 by incrementing 1`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+}
+
+console.log(`WAP to print numbers from 50 to 40 by decrementing 1`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+
+
+console.log(`WAP to find first 15 odd numbers`);
+for (let index = 1; (index <= 30); index = index + 2) {
+    console.log(index);
+}
+
+
+console.log(`WAP to find first 10 even numbers`);
+for (let index = 0; index < 20; index = index + 2) {
+     console.log(index);
+}
+
+console.log(`WAP to print table of 5`);
+for (let index = 5; index <=50 ; index = index + 5) {
+    console.log(index);    
+}
+
+console.log(`WAP to print table of 10`);
+for (let index = 10; index <=100 ; index = index + 10) {
+    console.log(index); 
+}
+
+console.log(`WAP to print table of 10 reverse order`);
+for (let index = 100; index >= 0 ; index = index - 10) {
+    console.log(index); 
+}
---
+++ 06_for_loop.js
@@ -0,0 +1,34 @@
+
+
+// Print number from 1 to 10
+//1. initialization 1    index=1
+//2. update Expression +1
+//3. condition <=10
+for (let index = 1; index <= 10; index = index + 1) {// index= 1 condition 1<=10
+    console.log(index); // 1    
+}
+
+for (let index = 0; index <= 5; index++) {
+    console.log(index);
+
+}
+console.log("Reverse number");
+// 10 to 0 
+//initial index = 10 
+for (let index = 10; index >= 0; index = index - 1) {
+    console.log(index);
+
+}
+
+console.log("Table of 5");
+for (let index = 5; index <= 50; index = index + 5) {
+    console.log(index);
+
+
+}
+
+// infinite loop 
+// console.log(`********************`);
+// for (let index = 1; index < 10;) {
+//     console.log(index);
+// }
