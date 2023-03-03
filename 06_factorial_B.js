03f36be2bedb95cf9c16fd698d8f8c5c1d050fe1
mitulborda987 <mitulborda987@gmail.com>
2023-03-03 17:30:02 +0530

    Update 06_factorial_B.js

--- 06_factorial_B.js
+++ 06_factorial_B.js
@@ -2,7 +2,8 @@
 
 function factorialofWordsCount(str) {
     if (str == null) {
-        return (`Given String ${str}. Please Enter valid Data`);
+        console.log(`Given String ${str}. Please Enter valid Data`);
+        return
     }
     let count = 1
     let facter = 1
@@ -13,30 +14,59 @@ function factorialofWordsCount(str) {
             facter = facter * count
         }
     }
-    // console.log(facter);
+    console.log(`The String ${str} Total Word: ${count}`);
+    console.log(`Factorial of given string is: ${facter}`);
     return count;
 }
 console.log(`---------------------------------------------------------------------`);
-let Result1 = factorialofWordsCount("Revision is the mother of Success")
-console.log(`The String "Revision is the mother of Success" Total Word: ${Result1}`);
-
+factorialofWordsCount("Revision is the mother of Success")
 console.log(`---------------------------------------------------------------------`);
-
-let Result2 = factorialofWordsCount("We naver fail, We always learn")
-console.log(`The String "We naver fail, We always learn" Total Word: ${Result2}`);
-
+factorialofWordsCount("We naver fail, We always learn")
 console.log(`---------------------------------------------------------------------`);
-
-let Result3 = factorialofWordsCount(null)
-console.log(`${Result3}`);
-
+factorialofWordsCount(null)
 console.log(`---------------------------------------------------------------------`);
-
-let Result4 = factorialofWordsCount("")
-console.log(`The String "" Total Word: ${Result4}`);
-
+factorialofWordsCount("")
 console.log(`---------------------------------------------------------------------`);
+factorialofWordsCount("Borda Mitul K")
 
-let Result5 = factorialofWordsCount("Borda Mitul K")
-console.log(`Borda Mitul K" Total Word: ${Result5}`);
+// console.log(`---------------------------------------------------------------------`);
 
+// let Result2 = factorialofWordsCount("We naver fail, We always learn")
+// console.log(`The String "We naver fail, We always learn" Total Word: ${Result2}`);
+
+// console.log(`---------------------------------------------------------------------`);
+
+// let Result3 = factorialofWordsCount(null)
+// console.log(`${Result3}`);
+
+// console.log(`---------------------------------------------------------------------`);
+
+// let Result4 = factorialofWordsCount("")
+// console.log(`The String "" Total Word: ${Result4}`);
+
+// console.log(`---------------------------------------------------------------------`);
+
+// let Result5 = factorialofWordsCount("Borda Mitul K")
+// console.log(`Borda Mitul K" Total Word: ${Result5}`);
+
+
+
+
+// function factorialofWordsCount(str) {
+//     if (str == null) {
+//         return (`Given String ${str}. Please Enter valid Data`);
+//     }
+//     let count = 1
+//     let facter = 1
+//     for (let index = 0; index < str.length; index++) {
+//         let char = str.charAt(index);
+//         if (char == " ") {
+//             count = count + 1
+//             facter = facter * count
+//         }
+//     }
+//     console.log(`The String ${str} Total Word: ${count}`);
+//     console.log(`Factorial of given string is: ${facter}`);
+//     return count;
+// }
+// factorialofWordsCount(`"Revision is the mother of Success"`)
